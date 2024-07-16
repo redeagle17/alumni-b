f1a77f8b09537c2619050544c25337c7e2b665fa
redeagle17 <ankursingh1438@gmail.com>
2024-07-16 13:59:42 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    await requestHandler(req, res, next);
+  } catch (error) {
+    res.status(error.code).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// const asyncHandler = (requestHandler) => {
+//   (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+export default asyncHandler;
