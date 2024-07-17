f567721eae5d818bd2d1d75cbcabb519592e7975
redeagle17 <ankursingh1438@gmail.com>
2024-07-17 20:51:01 +0530

    edge cases added in controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,44 +1,25 @@
 import { User } from "../models/user.model.js";
-import { UserProfile } from "../models/user.profile.model.js";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import asyncHandler from "../utils/asyncHandler.js";
 import errorHandler from "../utils/errorHandler.js";
 import responseHandler from "../utils/responseHandler.js";
 
-// const registerUser = asyncHandler(async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
-//   if (user) {
-//     throw new errorHandler(409, "User with email already exists");
-//   }
-
-//   const hashedPassword = await bcrypt.hash(password, 10);
-//   const newUser = new User({
-//     email: email,
-//     password: hashedPassword,
-//   });
-//   await newUser.save();
-//   const createdUser = {
-//     _id: newUser._id,
-//     email: newUser.email,
-//   };
-//   return res
-//     .status(201)
-//     .json(
-//       new responseHandler(200, createdUser, "User registered Successfully")
-//     );
-// });
-
 const registerUser = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
   if (!email || !password) {
     return next(new errorHandler(400, "Email and password are required"));
   }
+
+  if (password !== confirmPassword) {
+    return next(new errorHandler(400, "Password does not match"));
+  }
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     return next(new errorHandler(400, "Invalid email format"));
   }
+
   if (password.length < 6) {
     return next(
       new errorHandler(400, "Password must be at least 6 characters long")
@@ -47,11 +28,11 @@ const registerUser = asyncHandler(async (req, res, next) => {
 
   try {
     const normalizedEmail = email.toLowerCase();
-
     const user = await User.findOne({ email: normalizedEmail });
     if (user) {
       return next(new errorHandler(409, "User with this email already exists"));
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
@@ -76,4 +57,45 @@ const registerUser = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new errorHandler(400, "Email and password are required"));
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return next(new errorHandler(400, "Invalid email format"));
+  }
+
+  try {
+    const normalizedEmail = email.toLowerCase();
+    const user = await User.findOne({ email: normalizedEmail });
+    if (!user) {
+      return next(
+        new errorHandler(404, "User with this email id does not exists")
+      );
+    }
+
+    const hashedPassword = user.password;
+
+    if (bcrypt.compareSync(password, hashedPassword)) {
+      const createdUser = {
+        _id: user._id,
+        email: user.email,
+      };
+
+      return res
+        .status(200)
+        .json(
+          new responseHandler(200, createdUser, "User logged in successfully")
+        );
+    } else {
+      return next(new errorHandler(401, "Invalid user credentials"));
+    }
+  } catch (error) {
+    return next(new errorHandler(500, error.message));
+  }
+});
+
+export { registerUser, loginUser };
---
+++ src/controllers/user.profile.controller.js
@@ -0,0 +1,4 @@
+import { UserProfile } from "../models/user.profile.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import errorHandler from "../utils/errorHandler.js";
+import responseHandler from "../utils/responseHandler.js";
