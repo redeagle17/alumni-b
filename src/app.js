eb9d8eb0c7c43e8dccfa3e12d9c8dfecdbf58217
redeagle17 <ankursingh1438@gmail.com>
2024-07-20 13:34:31 +0530

    cors error fix v1

--- src/app.js
+++ src/app.js
@@ -19,6 +19,13 @@ app.use(
   })
 );
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(express.static("public"));
