4b5279df205a60e64edffb0ab27d9c1e2722b4a5
redeagle17 <ankursingh1438@gmail.com>
2024-07-20 12:53:33 +0530

    cors error

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
 
@@ -15,12 +16,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(express.static("public"));
 
+import userRouter from "./routes/user.route.js";
+import UserProfileRouter from "./routes/user.profile.route.js";
 
-import userRouter from './routes/user.route.js'
-import UserProfileRouter from './routes/user.profile.route.js'
-
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/users_data", UserProfileRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users_data", UserProfileRouter);
 
 app.use(errorMiddleware);
 
