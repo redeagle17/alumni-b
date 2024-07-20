0dde4c7778b82f698c589fb7c811637a6fb4a8f2
redeagle17 <ankursingh1438@gmail.com>
2024-07-20 13:42:39 +0530

    cors error fix v2

--- src/app.js
+++ src/app.js
@@ -4,28 +4,14 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-    methods: ["POST", "GET"],
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-    ],
-  })
-);
-
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
-
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  methods: ["POST", "GET"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(express.static("public"));
