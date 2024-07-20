994a2c9f14071d2c509ee758d1c25b1dc6e23b98
redeagle17 <ankursingh1438@gmail.com>
2024-07-20 20:29:46 +0530

    cloudinary error fixing (middleware added)

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
 
 import userRouter from "./routes/user.route.js";
 import UserProfileRouter from "./routes/user.profile.route.js";
